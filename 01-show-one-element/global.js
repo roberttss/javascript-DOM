d92e6a0750321c699c40061afab59c260989057a
roberttss <robertabolins772@gmail.com>
2021-03-23 14:47:57 +0200

    1komits more poga

--- 01-show-one-element/global.js
+++ 01-show-one-element/global.js
@@ -14,4 +14,13 @@ window.addEventListener("load", function(){
 
   // 5. Also modify the DOM to hide the "More info..." link.
   
+
+const more_text_link = document.getElementById('more_text_link')
+
+  more_text_link.addEventListener('click', function(e){
+    e.preventDefault()
+    var more_text_content = document.getElementById('more_text_content')
+    
+    more_text_content.style.display = 'block'
+  })
 });
